6e5a555dde743a6250d13242e80b4db5ea7bb38c
Hitesh Kumar Misro <hiteshkumarmisro@gmail.com>
2020-09-16 23:30:43 +0530

    Added mock json and modified code to load the products through GET call

--- src/js/app.js
+++ src/js/app.js
@@ -1,91 +1,75 @@
-var products = {
-    1: {
-        name: "Dining Table",
-        desc: "Table & Chairs",
-        img: "../../assets/images/table1.jpg",
-        price: 2034
-    },
-    2: {
-        name: "Table",
-        desc: "Table & Chairs",
-        img: "../../assets/images/table2.jpg",
-        price: 1247
-    },
-    3: {
-        name: "Sofa",
-        desc: "Lounge",
-        img: "../../assets/images/sofa2.jpg",
-        price: 675
-    },
-    4: {
-        name: "Sofa set",
-        desc: "Lounge",
-        img: "../../assets/images/sofa3.jpg",
-        price: 842
-    },
-    5: {
-        name: "Sofa with table",
-        desc: "Lounge",
-        img: "../../assets/images/sofa4.jpg",
-        price: 842
-    }
-};
+try {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 || this.status == 200 ||  this.status == 304) {
+            const response = JSON.parse(this.responseText);
+            loadProducts(response.products);
+        }
+    };
+    xhttp.open("GET", "../../product-data.json", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send();
+} catch (error) {
+    console.log(error);
+}
 
-/* PRODUCTS HTML GRID GENERATOR */
-window.addEventListener("load", function () {
-    var container = document.getElementById("cart-products"),
-        item = null, part = null;
-    for (let i in products) {
-        item = document.createElement("div");
-        item.classList.add("p-item", "col-md-4");
+/* LOAD PRODUCTS AFTER SUCCESS RESPONSE */
+let loadProducts = function (products) {
+    window.addEventListener("load", function () {
+        let container = document.getElementById("cart-products"),
+            item = null, part = null;
+        for (let i in products) {
+            item = document.createElement("div");
+            item.classList.add("p-item", "col-md-4");
 
-        // Product Image
-        partDiv = document.createElement("div");
-        part = document.createElement("img");
-        partDiv.appendChild(part);
-        part.src = products[i]['img'];
-        partDiv.classList.add("img-div");
-        part.classList.add("p-img");
-        item.appendChild(partDiv);
+            // Product Image
+            partDiv = document.createElement("div");
+            part = document.createElement("img");
+            partDiv.appendChild(part);
+            part.src = products[i]['img'];
+            partDiv.classList.add("img-div");
+            part.classList.add("p-img");
+            item.appendChild(partDiv);
 
-        // Product Name
-        part = document.createElement("div");
-        part.innerHTML = products[i]['name'];
-        part.classList.add("p-name");
-        item.appendChild(part);
+            // Product Name
+            part = document.createElement("div");
+            part.innerHTML = products[i]['name'];
+            part.classList.add("p-name");
+            item.appendChild(part);
 
-        // Product Price
-        part = document.createElement("div");
-        part.innerHTML = "$" + products[i]['price'];
-        part.classList.add("p-price");
-        item.appendChild(part);
+            // Product Price
+            part = document.createElement("div");
+            part.innerHTML = "$" + products[i]['price'];
+            part.classList.add("p-price");
+            item.appendChild(part);
 
-        // Product Description
-        part = document.createElement("div");
-        part.innerHTML = products[i]['desc'];
-        part.classList.add("p-desc");
-        item.appendChild(part);
+            // Product Description
+            part = document.createElement("div");
+            part.innerHTML = products[i]['desc'];
+            part.classList.add("p-desc");
+            item.appendChild(part);
 
-        // Add to cart
-        const cartIcon = `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-cart-plus" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
-                            <path fill-rule="evenodd" d="M8.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 .5-.5z"/>
-                            </svg>`;
+            // Add to cart
+            const cartIcon = `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-cart-plus" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+                                    <path fill-rule="evenodd" d="M8.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 .5-.5z"/>
+                                    </svg>`;
 
-        part = document.createElement("a");
-        part.href = "javascript:void(0)";
-        part.innerHTML = cartIcon;
-        part.classList.add("p-add");
-        part.onclick = cart.add;
-        part.dataset.id = i;
-        item.appendChild(part);
+            part = document.createElement("a");
+            part.href = "javascript:void(0)";
+            part.innerHTML = cartIcon;
+            part.classList.add("p-add");
+            part.onclick = cart.add;
+            part.dataset.id = i;
+            item.appendChild(part);
 
-        container.appendChild(item);
-    }
-});
+            container.appendChild(item);
+        }
+    });
+}
 
 /* SHOPPING CART */
-var cart = {
+let cart = {
     data: null, // current shopping cart
 
     /* [C1] LOCALSTORAGE */
@@ -108,7 +92,7 @@ var cart = {
         document.getElementById('products').style.display = "none";
     },
 
-    gotoProductsPage: function() {
+    gotoProductsPage: function () {
         document.getElementById('cart-list').style.display = "none";
         document.getElementById('products').style.display = "flex";
     },
@@ -117,7 +101,7 @@ var cart = {
     add: function () {
         // Update current cart
         if (cart.data[this.dataset.id] == undefined) {
-            var product = products[this.dataset.id];
+            let product = products[this.dataset.id];
             cart.data[this.dataset.id] = {
                 name: product['name'],
                 desc: product['desc'],
@@ -137,13 +121,13 @@ var cart = {
     list: function () {
         // list() : update HTML
 
-        var container = document.getElementById("cart-list"),
+        let container = document.getElementById("cart-list"),
             item = null, part = null, product = null;
         container.innerHTML = "";
 
         // Empty cart
-        var isempty = function (obj) {
-            for (var key in obj) {
+        let isempty = function (obj) {
+            for (let key in obj) {
                 if (obj.hasOwnProperty(key)) { return false; }
             }
             return true;
@@ -157,8 +141,8 @@ var cart = {
         // Not empty
         else {
             // List items
-            var total = 0, subtotal = 0;
-            for (var i in cart.data) {
+            let total = 0, subtotal = 0;
+            for (let i in cart.data) {
                 item = document.createElement("div");
                 item.classList.add("c-item", "col-md-12");
                 product = cart.data[i];
